100a986f815f741000f9fbc6ad2b8a16ae5b4955
Anes Cehic <anescehic@gmail.com>
2020-02-05 22:35:58 +0100

    Final version

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 var db = new Pool({
-  host: 'localhost',
+  host: 'ec2-54-195-252-243.eu-west-1.compute.amazonaws.com',
   port: 5432,
-  database: 'kvizopkn',
-  user: "postgres",
-  password: "a93mng9o",
+  database: 'd367uoep7tcs6t',
+  user: "gcqszkocfozibm",
+  password: "8e854f60170bf13c3592eb47a0ddbee4910fe66cf05fb1cb380e427cce151657",
 });
 
 module.exports = db
